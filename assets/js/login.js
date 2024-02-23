5e40084395696e61b1e6aabe7b0a19cc28250338
Altair015 <mand23sngh@gmail.com>
2024-02-23 21:16:17 +0530

    Created the login page and implemented the authentication as per the requirement. Created the Contact Us page. Created the Layout for the Cart Page.

---
+++ assets/js/login.js
@@ -0,0 +1,19 @@
+console.log(document.getElementById("loginSubmit"))
+
+function authEntication() {
+    let email = document.getElementById("emailId");
+    let pass = document.getElementById("passId");
+
+    if (email.value === "admin@admin.com" && pass.value === "123456") {
+        alert("Login successful");
+        window.location.reload()
+    }
+    else {
+        alert("Incorrect email or password")
+        window.location.reload()
+    }
+}
+
+let submitElement = document.getElementById("loginSubmit");
+
+submitElement.addEventListener("click", authEntication);
